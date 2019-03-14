bfdda2f582fe5acbc3e4e5370fcc6a5a41f204c7
J-Mo <j_mo321@yahoo.com>
2019-03-14 12:05:06 -0500

    Initial Commit

---
+++ Justin's Sweet Beats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Justin's Sweet Beats
+//
+//  Created by Justin Moore on 3/14/19.
+//  Copyright © 2019 Justin Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Justin's Sweet BeatsTests/Justin_s_Sweet_BeatsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Justin_s_Sweet_BeatsTests.swift
+//  Justin's Sweet BeatsTests
+//
+//  Created by Justin Moore on 3/14/19.
+//  Copyright © 2019 Justin Moore. All rights reserved.
+//
+
+import XCTest
+@testable import Justin_s_Sweet_Beats
+
+class Justin_s_Sweet_BeatsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
